db75cdbe136ea4733604ea7197597dd43caca280
Zoe Wong <89801077+zoetropew@users.noreply.github.com>
2023-01-05 16:46:26 -0800

    improvements to readability

--- MovieWallDriver.java
+++ MovieWallDriver.java
@@ -6,10 +6,12 @@ public class MovieWallDriver {
 		
 		UserInterface ui = new UserInterface();
 		
+		System.out.println("===========================");
 		System.out.println("Welcome to the Movie Wall!");
 		while(!ui.askUser(database.getDatabase())) {} // prompts the user for input until they type EXIT
 		ui.closeScanner(); // closes the user input scanner
 		System.out.println("Thank you for using the Movie Wall!");
+		System.out.println("=====================================");
 
 	}
 
